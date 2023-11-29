b4a9e2f17524801234f7074ceaa019ec85338a7d
AndresPozu <andres.sirvent@gmail.com>
2023-11-29 11:29:09 +0100

    Corregidos errores lógicos

--- PracticaGit2324/Form1.cs
+++ PracticaGit2324/Form1.cs
@@ -19,34 +19,40 @@ namespace PracticaGit2324
 
         private void button1_Click(object sender, EventArgs e)
         {
-            string textoTelegrama;
-            char tipoTelegrama = ' ';
+            string textoTelegrama = txtTelegrama.Text;
             int numPalabras = 0;
-            double coste;
-            //Leo el telegrama
-            textoTelegrama = txtTelegrama.Text;
-            // telegrama urgente?
+            double coste = 0;
+            // Obtener el número de palabras que forma el telegrama
+            char[] delimitadores = new char[] { ' ', '\r', '\n' };
+            numPalabras = textoTelegrama.Split(delimitadores,
+            StringSplitOptions.RemoveEmptyEntries).Length;
+            // Calcular el costo del telegrama
             if (cbUrgente.Checked)
-                tipoTelegrama = 'u';
-            //Obtengo el número de palabras que forma el telegrama 
-            numPalabras = textoTelegrama.Length;
-            //Si el telegrama es ordinario
-            if (tipoTelegrama == 'o')
+            {
+                // Telegrama urgente
                 if (numPalabras <= 10)
-                    coste = 25;
-                else
-                    coste = 0.5 * numPalabras;
-            else
-            //Si el telegrama es urgente
-            if (tipoTelegrama == 'u')
-                if (numPalabras <= 10)
+                {
                     coste = 5;
+                }
                 else
+                {
                     coste = 5 + 0.75 * (numPalabras - 10);
+                }
+            }
             else
-                coste = 0;
-            txtPrecio.Text = coste.ToString() + " euros";
-
+            {
+                // Telegrama ordinario
+                if (numPalabras <= 10)
+                {
+                    coste = 2.5;
+                }
+                else
+                {
+                    coste = 2.5 + 0.5 * (numPalabras - 10);
+                }
+            }
+            // Mostrar el costo en el TextBox de precio
+            txtPrecio.Text = coste.ToString("C") + " euros"; // "C" formatea como moneda
         }
     }
 }
